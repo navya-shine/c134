c71ec65e43bb504ebd7ede4067ba7332629fc058
navya-shine <69377970+navya-shine@users.noreply.github.com>
2021-07-03 15:59:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+img = "";
+status = "";
+objects = [];
+
+function preload()
+{
+    img = loadImage('dog_cat.jpg');
+}
+function setup()
+{
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    video.size();
+    objectDetecter = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Staus : Dectecting Objects";
+}
+function modelLoaded()
+{
+    console.log('Model Loaded');
+    status = true;
+    
+}
+function draw()
+{
+    image(video,0,0,380,380);
+
+    if(status !="")
+    {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetecter.detect(video , gotPoses);
+
+        for(i=0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Staus : Dectected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are :" + objects.length;
+            
+            fill(r,g,b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%" , objects[i].x,objects[i].y);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+}
+function gotPoses(error, results)
+{
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
